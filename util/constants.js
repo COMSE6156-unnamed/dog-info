5c64f160eae8891be84f49e4fa1e63620b984dde
hsingwenhsu <hh2916@columbia.edu>
2022-11-11 20:29:23 -0500

    removed paginationRelationFirst from constants file

--- util/constants.js
+++ util/constants.js
@@ -12,7 +12,6 @@ const paginationRelationNext = "next";
 const constants = {
     defaultPageSize,
     defaultOffset,
-    paginationRelationFirst,
     paginationRelationCurrent,
     paginationRelationPrev,
     paginationRelationNext,
